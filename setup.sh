f8b3c6ddc471b4f2a28e6babb75a8de5eca33271
Daelyn Jones <daelyndbjones@gmail.com>
2021-11-10 22:13:31 -0800

    Add pgcli to setup

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ brew install nginx
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 brew install postgres
 /usr/local/opt/postgresql/bin/postgres -D /usr/local/var/postgres
+brew install pgcli 
