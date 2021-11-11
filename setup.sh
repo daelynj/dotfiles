ab4953cf645c9966a3afe7972742e513f996607c
Daelyn Jones <daelyndbjones@gmail.com>
2021-11-10 22:05:55 -0800

    Add ohmyzsh to setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install tmux
 brew install nginx
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
