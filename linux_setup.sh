99c052215824b3939fbd1570f5e6c4687541d16d
Daelyn Jones <daelyndbjones@gmail.com>
2021-11-11 16:51:04 -0800

    Add linux setup

---
+++ linux_setup.sh
@@ -0,0 +1,17 @@
+sudo apt update
+sudo apt upgrade
+
+sudo apt install zsh
+chsh -s $(which zsh)
+
+sudo apt install tmux
+
+sudo apt install nginx
+
+sudo apt install postgres
+
+sudo apt install pgcli
+
+sudo apt install fzy
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
