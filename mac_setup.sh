82c2670ea7468be75177b8d35d7950e98b6081de
Daelyn Jones <daelyndbjones@gmail.com>
2021-11-11 18:25:28 -0800

    Add comments

--- mac_setup.sh
+++ mac_setup.sh
@@ -1,8 +1,18 @@
+# Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 brew install tmux
+
 brew install nginx
+
+# Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 brew install postgres
+
+# Start postgres on launch
 /usr/local/opt/postgresql/bin/postgres -D /usr/local/var/postgres
+
 brew install pgcli 
+
 brew install fzy
